aacf3ee5b3e14f91af9fcbb0341d4ce3c378897d
公司-温鑫 <986315020@qq.com>
2018-07-27 16:48:14 +0800

    Default

--- CONFIG.py
+++ CONFIG.py
@@ -1,1 +1,7 @@
-CRRPID = "dingc7fb31db15999b4435c2f4657eb6378f"
+'常量配置 '
+
+'CORP_ID 是公司识别码'
+CORP_ID = "dingc7fb31db15999b4435c2f4657eb6378f"
+
+'CORP_SECRET 钉钉中考勤日报这个应用的识别码'
+CORP_SECRET = "FWGnzL06BnOh8S7vOctAYgvSumOuxQpE7Bc6wqdwCc4qdyP2uSRO0w7C9ggstocO"
---
+++ main.py
@@ -0,0 +1,10 @@
+'自动生成考勤日报并发布到微信群中'
+
+import requests
+
+'-API调用须知-'
+'调用API前需获取Access_Token'
+'当企业应用服务调用钉钉开放平台接口时'
+'需使用https协议、Json数据格式、UTF8编码'
+'域名为https://oapi.dingtalk.com'
+'POST请求请在HTTP Header中设置 Content-Type:application/json，否则接口调用失败'
