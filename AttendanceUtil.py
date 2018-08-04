6f172144c1d2864e9479715af33c2e9d22396890
wenxin <wenxin@appnav.cn>
2018-08-04 10:23:53 +0800

    获取公司所有人员信息

--- AttendanceUtil.py
+++ AttendanceUtil.py
@@ -1,7 +1,7 @@
-'''
+"""
 用于获取各项考勤数据的类
 BY LEO 2018-8-3
-'''
+"""
 
 import requests
 "**********************************README**************************************"
@@ -16,19 +16,21 @@ import requests
 '*/LEO 2018.7.30/'
 '******************************************************************************'
 
-class AttendanceUtil():
-    '考勤工具类'
+
+class AttendanceUtil:
 
     # 常量
-    __API_URL = 'https://oapi.dingtalk.com/'    # 接口域名
-    __API_GETTOKEN = 'gettoken'                   # API - 获取accesstoken的api
-    __API_DEPARTMENT_LIST_IDS = 'department/list_ids' # API - 获取部门id列表的api
-    __PARAM_CORPID = 'corpid'                     # CORP_ID     参数名称
-    __PARAM_CORPSECRET = 'corpsecret'             # CORP_SECRET 参数名称
-    __PARAM_ACCESS_TOKEN = 'access_token'         # accesstoken 参数名称
-    __PARAM_ID = 'id'                              # id 参数名称（父部门）
-    __PARAM_DEPARTMENT_ID = 'department_id'        # department_id 参数名称（当前部门）
-    __PARAM_SUB_DEPT_ID_LIST = 'sub_dept_id_list'
+    __API_URL = 'https://oapi.dingtalk.com/'             # 接口域名
+    __API_GETTOKEN = 'gettoken'                          # API - 获取access token的api
+    __API_DEPARTMENT_LIST_IDS = 'department/list_ids'    # API - 获取部门id列表的api
+    __API_USER_SIMPLELIST = 'user/simplelist'            # API - 获取部门人员简单信息的api
+    __PARAM_CORPID = 'corpid'                            # PARAM - CORPID 参数名称
+    __PARAM_CORPSECRET = 'corpsecret'                    # PARAM - CORPSECRET 参数名称
+    __PARAM_ACCESS_TOKEN = 'access_token'                # PARAM - access token 参数名称
+    __PARAM_ID = 'id'                                    # PARAM - id 参数名称（父部门）
+    __PARAM_DEPARTMENT_ID = 'department_id'              # PARAM - department_id 参数名称（当前部门）
+    __JSON_PARAM_SUB_DEPT_ID_LIST = 'sub_dept_id_list'   # JSON_PARAM - 参数名称（部门列表）
+    __JSON_USERLIST = 'userlist'                         # JSON_PARAM - 参数名称（用户简单列表）
 
     # 变量
     __CORP_ID = ''        # CORP_ID 是公司识别码
@@ -41,8 +43,8 @@ class AttendanceUtil():
 
     '公有 - 刷新accessToken的方法'
     def refreshAccessToken(self):
-        payload = {self.__PARAM_CORPID : self.__CORP_ID, self.__PARAM_CORPSECRET : self.__CORP_SECRET}
-        response = requests.get(self._getfinalAPI(self.__API_GETTOKEN), params = payload)
+        payload = {self.__PARAM_CORPID: self.__CORP_ID, self.__PARAM_CORPSECRET: self.__CORP_SECRET}
+        response = requests.get(self._getfinalAPI(self.__API_GETTOKEN), params=payload)
         self.__AccessToken = response.json()[self.__PARAM_ACCESS_TOKEN]
         return
 
@@ -52,20 +54,21 @@ class AttendanceUtil():
 
     '公有 - 获取所有部门id的方法'
     def getDepartmentIds(self, fatherDepartment = 1):
-        payload = {self.__PARAM_ACCESS_TOKEN : self.__AccessToken, self.__PARAM_ID : fatherDepartment}
-        response = requests.get(self._getfinalAPI(self.__API_DEPARTMENT_LIST_IDS), params = payload)
-        return response.json()[self.__PARAM_SUB_DEPT_ID_LIST]
+        payload = {self.__PARAM_ACCESS_TOKEN: self.__AccessToken, self.__PARAM_ID: fatherDepartment}
+        response = requests.get(self._getfinalAPI(self.__API_DEPARTMENT_LIST_IDS), params=payload)
+        return response.json()[self.__JSON_PARAM_SUB_DEPT_ID_LIST]
 
     '公有 - 获取当前部门的所有用户的简单信息的方法'
     def getCurDepartmentUserSimpleInfo(self, department_id):
-        payload = {self.__PARAM_ACCESS_TOKEN : self.__AccessToken, self.__PARAM_DEPARTMENT_ID : department_id}
-        requests.get(self._getfinalAPI())
+        payload = {self.__PARAM_ACCESS_TOKEN: self.__AccessToken, self.__PARAM_DEPARTMENT_ID: department_id}
+        response = requests.get(self._getfinalAPI(self.__API_USER_SIMPLELIST), params=payload)
+        return response.json()[self.__JSON_USERLIST]
 
     '构造方法'
-    def __init__(self, CORP_ID, CORP_SECRET):
+    def __init__(self, corp_id, corp_secret):
         # 初始化两个识别码
-        self.__CORP_ID = CORP_ID
-        self.__CORP_SECRET = CORP_SECRET
+        self.__CORP_ID = corp_id
+        self.__CORP_SECRET = corp_secret
         # 获取AccessToken
         self.refreshAccessToken()
         return
--- main.py
+++ main.py
@@ -1,19 +1,8 @@
 '自动生成考勤日报并发布到微信群中'
 
-import CONFIGS
+from AttendanceUtil import AttendanceUtil
 import requests
-import json
-"**********************************README**************************************"
-'-API调用须知-'
-'调用API前需获取Access_Token'
-'Access_Token有效期为7200秒，到期请重新获取，未到期重新获取将自动续期'
 
-'当企业应用服务调用钉钉开放平台接口时'
-'需使用https协议、Json数据格式、UTF8编码'
-'域名为https://oapi.dingtalk.com'
-'POST请求请在HTTP Header中设置 Content-Type:application/json，否则接口调用失败'
-'*/LEO 2018.7.30/'
-'******************************************************************************'
 
 '********************************常量配置***************************************'
 'CORP_ID 是公司识别码'
@@ -22,39 +11,35 @@ CORP_ID = "dingc7fb31db15999b4435c2f4657eb6378f"
 'CORP_SECRET 钉钉中考勤日报这个应用的识别码'
 CORP_SECRET = "FWGnzL06BnOh8S7vOctAYgvSumOuxQpE7Bc6wqdwCc4qdyP2uSRO0w7C9ggstocO"
 
-'接口域名'
-URL = 'https://oapi.dingtalk.com/'
 '*******************************************************************************'
 
-'******函数*******'
-def getAPI_URL(api):
-    return URL + api
-'*****************'
 
-print('开始获取access_token')
-'构建GET请求参数'
-payload = {'corpid' : CORP_ID, 'corpsecret' : CORP_SECRET}
-'发起GET请求'
-response = requests.get(getAPI_URL('gettoken'), params = payload)
-print('access_token获取结果:' + response.text)
-dict_response = response.json()
-'提取AccessToken'
-access_token = dict_response['access_token']
-print('解析得到access_token: '  + access_token)
+def getUniquList(unUniqueList):
+    uniqueList = []
+    for element in unUniqueList:
+        if element not in uniqueList:
+            uniqueList.append(element)
+    return uniqueList
 
-print('开始获取部门id_list')
-payload = {'access_token' : access_token, 'id' : '1'}
-response = requests.get(getAPI_URL('department/list_ids'), params=payload)
-print('部门id获取结果')
-dict_response = response.json()
-dapartment_listIds = dict_response['sub_dept_id_list']
-print(dapartment_listIds)
 
-userLists = []
-for dapartment_id in dapartment_listIds:
-    payload = {'access_token' : access_token, 'department_id' : dapartment_id}
-    response = requests.get(getAPI_URL('user/simplelist'),params=payload)
-    userList = (response.json())['userlist']
-    userLists = userLists + userList
+ouyeelAttendance = AttendanceUtil(CORP_ID, CORP_SECRET)
+print('ACCSESS_TOKEN: ' + ''.join(ouyeelAttendance.getAccessToken()))
 
-print(userLists)
+'获取公司所有部门'
+departmentIdList = ouyeelAttendance.getDepartmentIds()
+print('部门ID: ' + ' '.join(list(map(str, departmentIdList))))
+
+'获取每个部门下的员工合集'
+userSimpleInfoList = []
+for departmentId in departmentIdList:
+    userSimpleInfoList.extend(ouyeelAttendance.getCurDepartmentUserSimpleInfo(departmentId))
+'去重'
+userSimpleInfoList = getUniquList(userSimpleInfoList)
+print('公司人员信息条数：' + str(len(userSimpleInfoList)))
+
+'获取所有id合集'
+userIdList = []
+for userIdDic in userSimpleInfoList:
+    userIdList.append(userIdDic['userid'])
+print('所有人员id条数：' + str(len(userIdList)))
+
