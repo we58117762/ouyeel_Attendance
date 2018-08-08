591e300a88e443d19a18b9f647f36088b7f2a30e
公司-温鑫 <986315020@qq.com>
2018-08-08 16:13:45 +0800

    Default

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ CORP_ID = "dingc7fb31db15999b4435c2f4657eb6378f"
 'CORP_SECRET 钉钉中考勤日报这个应用的识别码'
 CORP_SECRET = "FWGnzL06BnOh8S7vOctAYgvSumOuxQpE7Bc6wqdwCc4qdyP2uSRO0w7C9ggstocO"
 
-#BEGIN_TIME = time.strftime("%Y-%m-%d 00:00:00", time.localtime())
-#END_TIME = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+BEGIN_TIME = time.strftime("%Y-%m-%d 00:00:00", time.localtime())
+END_TIME = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
-BEGIN_TIME = "2018-08-06 00:00:00"
-END_TIME = "2018-08-06 23:59:59"
+#BEGIN_TIME = "2018-08-08 00:00:00"
+#END_TIME = "2018-08-08 23:59:59"
 
 '*******************************************************************************'
 print(BEGIN_TIME)
@@ -39,7 +39,7 @@ departmentIdList = ouyeelAttendance.getDepartmentIds()
 print('\n②获取所有部门ID: ')
 print(departmentIdList)
 
-'获取每个部门下的员工合集'
+'获取每个部门下的员工姓名及id合集'
 userSimpleInfoList = []
 for departmentId in departmentIdList:
     userSimpleInfoList.extend(ouyeelAttendance.getCurDepartmentUserSimpleInfo(departmentId))
@@ -52,9 +52,9 @@ userIdList = []
 userSimpleInfoDicList = userSimpleInfoList
 for userSimpleInfoDic in userSimpleInfoList:
     userIdList.append(userSimpleInfoDic['userid'])
-print(userSimpleInfoDicList)
+#print(userSimpleInfoDicList)
 
-'获取考勤信息'
+'根据id获取考勤结果'
 attendanceInfoDicList = ouyeelAttendance.getUserAttendanceInfo(
     userIdList,
     BEGIN_TIME,
@@ -63,7 +63,7 @@ print('\n获取考勤信息：')
 print('考勤信息条数：' + str(len(attendanceInfoDicList)))
 #print(attendanceInfoDicList)
 
-'将考勤信息和人员对应起来'
+'将考勤结果和人员对应起来'
 formatAttendanceInfoDicList = []
 for attendanceInfoDic in attendanceInfoDicList:
     for userSimpleInfoDic in userSimpleInfoDicList:
@@ -73,12 +73,13 @@ for attendanceInfoDic in attendanceInfoDicList:
             formatAttendanceInfoDicList.append(tempAttendanceInfoDic)
 print('\n获取格式化考勤信息：')
 print('共有' + str(len(formatAttendanceInfoDicList)))
-print(formatAttendanceInfoDicList)
+#print(formatAttendanceInfoDicList)
 
-'生成考勤报表'
-resultString = []
+'将考勤结果有意义化'
+localiseAttendanceInfoDicList = []
 for formatAttendanceInfoDic in formatAttendanceInfoDicList:
-    resultString.append(
-        formatAttendanceInfoDic['name'] + ' '
-    )
-print(resultString)
+    tempDic = {
+        'name': formatAttendanceInfoDic['name'],
+
+    }
+
