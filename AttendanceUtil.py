c0b34157765b10b5a277d64074281d48fac54356
公司-温鑫 <986315020@qq.com>
2018-08-09 13:51:38 +0800

    Default

--- AttendanceUtil.py
+++ AttendanceUtil.py
@@ -21,28 +21,34 @@ import json
 class AttendanceUtil:
 
     # 常量
-    __API_URL = 'https://oapi.dingtalk.com/'             # 接口域名
-    __API_GETTOKEN = 'gettoken'                          # API - 获取access token的api
-    __API_DEPARTMENT_LIST_IDS = 'department/list_ids'    # API - 获取部门id列表的api
-    __API_USER_SIMPLELIST = 'user/simplelist'            # API - 获取部门人员简单信息的api
-    __API_ATTENDANCE_LIST = 'attendance/list'            # API - 获取考勤信息
+    API_URL = 'https://oapi.dingtalk.com/'             # 接口域名
+    API_GETTOKEN = 'gettoken'                          # API - 获取access token的api
+    API_DEPARTMENT_LIST_IDS = 'department/list_ids'    # API - 获取部门id列表的api
+    API_USER_SIMPLELIST = 'user/simplelist'            # API - 获取部门人员简单信息的api
+    API_ATTENDANCE_LIST = 'attendance/list'            # API - 获取考勤信息
 
-    __PARAM_CORPID = 'corpid'                            # PARAM - CORPID 参数名称
-    __PARAM_CORPSECRET = 'corpsecret'                    # PARAM - CORPSECRET 参数名称
-    __PARAM_ACCESS_TOKEN = 'access_token'                # PARAM - access token 参数名称
-    __PARAM_ID = 'id'                                    # PARAM - id 参数名称（父部门）
-    __PARAM_DEPARTMENT_ID = 'department_id'              # PARAM - department_id 参数名称（当前部门）
+    PARAM_CORPID = 'corpid'                            # PARAM - CORPID 参数名称
+    PARAM_CORPSECRET = 'corpsecret'                    # PARAM - CORPSECRET 参数名称
+    PARAM_ACCESS_TOKEN = 'access_token'                # PARAM - access token 参数名称
+    PARAM_ID = 'id'                                    # PARAM - id 参数名称（父部门）
+    PARAM_DEPARTMENT_ID = 'department_id'              # PARAM - department_id 参数名称（当前部门）
 
-    __JSON_PARAM_SUB_DEPT_ID_LIST = 'sub_dept_id_list'   # JSON_PARAM - 参数名称（部门列表）
-    __JSON_USERLIST = 'userlist'                         # JSON_PARAM - 参数名称（用户简单列表）
-    __JSON_USERIDS = 'userIds'                           # JSON_PARAM - 用户ID列表
-    __JSON_WORK_DATE_FROM = 'workDateFrom'               # JSON_PARAM - 考勤开始时间
-    __JSON_WORK_DATE_TO = 'workDateTo'                   # JSON_PARAM - 考勤结束时间
-    __JSON_RECORD_RESULT = 'recordresult'                # JSON_PARAM - 考勤结果
-    __JSON_HAS_MORE = 'hasMore'                          # JSON_PARAM - 表明是否还有更多考勤数据
-    __JSON_OFFSET = 'offset'                             # JSON_PARAM - 考勤结果读取偏移量
-    __JSON_LIMIT = 'limit'                               # JSON_PARAM - 一次读取的结果条数
-
+    JSON_PARAM_SUB_DEPT_ID_LIST = 'sub_dept_id_list'   # JSON_PARAM - 参数名称（部门列表）
+    JSON_USERLIST = 'userlist'                         # JSON_PARAM - 参数名称（用户简单列表）
+    JSON_USER_ID = 'userid'
+    JSON_USERID = 'userId'
+    JSON_USERIDS = 'userIds'
+    JSON_NAME = 'name'                                 # JSON_PARAM - 用户ID列表
+    JSON_WORK_DATE_FROM = 'workDateFrom'               # JSON_PARAM - 考勤开始时间
+    JSON_WORK_DATE_TO = 'workDateTo'                   # JSON_PARAM - 考勤结束时间
+    JSON_RECORD_RESULT = 'recordresult'                # JSON_PARAM - 考勤结果
+    JSON_HAS_MORE = 'hasMore'                          # JSON_PARAM - 表明是否还有更多考勤数据
+    JSON_OFFSET = 'offset'                             # JSON_PARAM - 考勤结果读取偏移量
+    JSON_LIMIT = 'limit'                               # JSON_PARAM - 一次读取的结果条数
+    JSON_CHECK_TYPE = 'checkType'
+    JSON_CHECK_TYPE_ONDUTY = 'OnDuty'
+    JSON_CHECK_TYPE_OFF_DUTY = 'OffDuty'
+    JSON_TIME_RESULT = 'timeResult'
 
     # 变量
     __CORP_ID = ''        # CORP_ID 是公司识别码
@@ -58,17 +64,18 @@ class AttendanceUtil:
 
     '私有 - 拼接api地址的方法'
     def _getfinalAPI(self, api):
-        return self.__API_URL + api
+        return self.API_URL + api
 
     '私有 - 拼接api地址的方法POST'
     def _getfinalAPI_post(self, api):
-        return self.__API_URL + api + '?access_token=' + self.__AccessToken
+        return self.API_URL + api + '?access_token=' + self.__AccessToken
 
     '公有 - 刷新accessToken的方法'
     def refreshAccessToken(self):
-        payload = {self.__PARAM_CORPID: self.__CORP_ID, self.__PARAM_CORPSECRET: self.__CORP_SECRET}
-        response = requests.get(self._getfinalAPI(self.__API_GETTOKEN), params=payload)
-        self.__AccessToken = response.json()[self.__PARAM_ACCESS_TOKEN]
+        #重新获取accessToken#
+        payload = {self.PARAM_CORPID: self.__CORP_ID, self.PARAM_CORPSECRET: self.__CORP_SECRET}
+        response = requests.get(self._getfinalAPI(self.API_GETTOKEN), params=payload)
+        self.__AccessToken = response.json()[self.PARAM_ACCESS_TOKEN]
         return
 
     '公有 - 读取accessToken的方法'
@@ -77,15 +84,15 @@ class AttendanceUtil:
 
     '公有 - 获取所有部门id的方法'
     def getDepartmentIds(self, fatherDepartment = 1):
-        payload = {self.__PARAM_ACCESS_TOKEN: self.__AccessToken, self.__PARAM_ID: fatherDepartment}
-        response = requests.get(self._getfinalAPI(self.__API_DEPARTMENT_LIST_IDS), params=payload)
-        return response.json()[self.__JSON_PARAM_SUB_DEPT_ID_LIST]
+        payload = {self.PARAM_ACCESS_TOKEN: self.__AccessToken, self.PARAM_ID: fatherDepartment}
+        response = requests.get(self._getfinalAPI(self.API_DEPARTMENT_LIST_IDS), params=payload)
+        return response.json()[self.JSON_PARAM_SUB_DEPT_ID_LIST]
 
     '公有 - 获取当前部门的所有用户的简单信息的方法'
     def getCurDepartmentUserSimpleInfo(self, department_id):
-        payload = {self.__PARAM_ACCESS_TOKEN: self.__AccessToken, self.__PARAM_DEPARTMENT_ID: department_id}
-        response = requests.get(self._getfinalAPI(self.__API_USER_SIMPLELIST), params=payload)
-        return response.json()[self.__JSON_USERLIST]
+        payload = {self.PARAM_ACCESS_TOKEN: self.__AccessToken, self.PARAM_DEPARTMENT_ID: department_id}
+        response = requests.get(self._getfinalAPI(self.API_USER_SIMPLELIST), params=payload)
+        return response.json()[self.JSON_USERLIST]
 
     '公有 - 获取用户考勤信息的方法'
     def getUserAttendanceInfo(self, userIdList, beginTime, endTime):
@@ -98,17 +105,17 @@ class AttendanceUtil:
         limit = 50
         response = []
         while True:
-            data = {self.__JSON_WORK_DATE_FROM: beginTime,
-                    self.__JSON_WORK_DATE_TO: endTime,
-                    self.__JSON_USERIDS: userIdList,
-                    self.__JSON_OFFSET: offset,
-                    self.__JSON_LIMIT: limit}
-            r = requests.post(self._getfinalAPI_post(self.__API_ATTENDANCE_LIST),
-                                     headers=headers,
-                                     json=data)
-            response.extend(r.json()[self.__JSON_RECORD_RESULT])
+            data = {self.JSON_WORK_DATE_FROM: beginTime,
+                    self.JSON_WORK_DATE_TO: endTime,
+                    self.JSON_USERIDS: userIdList,
+                    self.JSON_OFFSET: offset,
+                    self.JSON_LIMIT: limit}
+            r = requests.post(self._getfinalAPI_post(self.API_ATTENDANCE_LIST),
+                              headers=headers,
+                              json=data)
+            response.extend(r.json()[self.JSON_RECORD_RESULT])
             offset += limit
-            if r.json()[self.__JSON_HAS_MORE] is False:
+            if r.json()[self.JSON_HAS_MORE] is False:
                 break
         return response
 
--- main.py
+++ main.py
@@ -12,22 +12,75 @@ CORP_ID = "dingc7fb31db15999b4435c2f4657eb6378f"
 'CORP_SECRET 钉钉中考勤日报这个应用的识别码'
 CORP_SECRET = "FWGnzL06BnOh8S7vOctAYgvSumOuxQpE7Bc6wqdwCc4qdyP2uSRO0w7C9ggstocO"
 
-BEGIN_TIME = time.strftime("%Y-%m-%d 00:00:00", time.localtime())
-END_TIME = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+#BEGIN_TIME = time.strftime("%Y-%m-%d 00:00:00", time.localtime())
+#END_TIME = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 
-#BEGIN_TIME = "2018-08-08 00:00:00"
-#END_TIME = "2018-08-08 23:59:59"
+BEGIN_TIME = "2018-08-08 00:00:00"
+END_TIME = "2018-08-08 23:59:59"
 
 '*******************************************************************************'
 print(BEGIN_TIME)
 print(END_TIME)
 
+'辅助 - 列表排重'
 def getUniquList(unUniqueList):
     uniqueList = []
     for element in unUniqueList:
         if element not in uniqueList:
             uniqueList.append(element)
     return uniqueList
+
+
+'辅助 - 翻译考勤结果字符串'
+def formatAttendanceResult(attendanceResult):
+    """时间结果翻译"""
+    resultDic = {
+        'Normal': '正常',
+        'Early': '早退',
+        'Late': '迟到',
+        'SeriousLate': '严重迟到',
+        'Absenteeism': '旷工迟到',
+        'NotSigned': '未打卡'
+    }
+
+    if attendanceResult in resultDic:
+        result = resultDic[attendanceResult]
+    else:
+        result = '未定义'
+
+    return result
+
+
+
+
+
+    return
+
+
+'主要 - 获取考勤结果信息（不含有市外出差、市内外出及请假信息 - 只能获取到打过卡的人的记录）'
+def getAttendanceResult_OnlyChecked(userIdList, userSimpleInfoDicList, beginTime, endTime):
+    '根据id获取考勤结果(此考勤)'
+    attendanceInfoDicList = ouyeelAttendance.getUserAttendanceInfo(
+        userIdList,
+        beginTime,
+        endTime)
+    print('\n获取考勤信息：')
+    print('考勤信息条数：' + str(len(attendanceInfoDicList)))
+
+    '将考勤结果和人员对应起来（仅保留上班考勤）'
+    formatAttendanceInfoDicList = []
+    for attendanceInfoDic in attendanceInfoDicList:
+        '仅保留上班考勤记录'
+        if attendanceInfoDic[ouyeelAttendance.JSON_CHECK_TYPE] == ouyeelAttendance.JSON_CHECK_TYPE_OFF_DUTY:
+            continue
+        for userSimpleInfoDic in userSimpleInfoDicList:
+            if userSimpleInfoDic[ouyeelAttendance.JSON_USER_ID] == attendanceInfoDic[ouyeelAttendance.JSON_USERID]:
+                tempAttendanceInfoDic = attendanceInfoDic
+                tempAttendanceInfoDic[ouyeelAttendance.JSON_NAME] = userSimpleInfoDic[ouyeelAttendance.JSON_NAME]
+                formatAttendanceInfoDicList.append(tempAttendanceInfoDic)
+    print('\n获取格式化考勤信息：')
+    print('共有' + str(len(formatAttendanceInfoDicList)))
+    return formatAttendanceInfoDicList
 
 
 ouyeelAttendance = AttendanceUtil(CORP_ID, CORP_SECRET)
@@ -50,36 +103,15 @@ print('信息条数为(去重后):' + str(len(userSimpleInfoList)))
 '获取所有姓名及id对合集'
 userIdList = []
 userSimpleInfoDicList = userSimpleInfoList
-for userSimpleInfoDic in userSimpleInfoList:
-    userIdList.append(userSimpleInfoDic['userid'])
-#print(userSimpleInfoDicList)
+for userSimpleInfoDic in userSimpleInfoDicList:
+    userIdList.append(userSimpleInfoDic[ouyeelAttendance.JSON_USER_ID])
 
-'根据id获取考勤结果'
-attendanceInfoDicList = ouyeelAttendance.getUserAttendanceInfo(
-    userIdList,
-    BEGIN_TIME,
-    END_TIME)
-print('\n获取考勤信息：')
-print('考勤信息条数：' + str(len(attendanceInfoDicList)))
-#print(attendanceInfoDicList)
+attendanceInfo_onlyChecked_DicList = getAttendanceResult_OnlyChecked(userIdList, userSimpleInfoDicList, BEGIN_TIME, END_TIME)
+#print(attendanceInfo_onlyChecked_DicList)
 
-'将考勤结果和人员对应起来'
-formatAttendanceInfoDicList = []
-for attendanceInfoDic in attendanceInfoDicList:
-    for userSimpleInfoDic in userSimpleInfoDicList:
-        if userSimpleInfoDic['userid'] == attendanceInfoDic['userId']:
-            tempAttendanceInfoDic = attendanceInfoDic
-            tempAttendanceInfoDic['name'] = userSimpleInfoDic['name']
-            formatAttendanceInfoDicList.append(tempAttendanceInfoDic)
-print('\n获取格式化考勤信息：')
-print('共有' + str(len(formatAttendanceInfoDicList)))
-#print(formatAttendanceInfoDicList)
 
-'将考勤结果有意义化'
-localiseAttendanceInfoDicList = []
-for formatAttendanceInfoDic in formatAttendanceInfoDicList:
-    tempDic = {
-        'name': formatAttendanceInfoDic['name'],
+'仅保留'
 
-    }
+
+
 
